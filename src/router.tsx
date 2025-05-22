a39613c36ef3cc124cd084660ddbd7e69eb4dab1
Joan Manel Sancho Garcia <joanmanelsanchogarcia@MacBook-Pro-de-Joan.local>
2025-05-22 10:27:09 +0200

    add Lazy

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
 import FavoritesPage from "./views/FavoritesPage"
@@ -8,8 +9,17 @@ export default function AppRouter() {
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<IndexPage />} index />
-                    <Route path="/favorites" element={<FavoritesPage />} />
+
+                    <Route path="/" element={
+                        <Suspense fallback="Loading...">
+                            <IndexPage />
+                        </Suspense>
+                    } index />
+                    <Route path="/favorites" element={
+                        <Suspense fallback="Loading...">
+                            <FavoritesPage />
+                        </Suspense>
+                    } />
                 </Route>
             </Routes>
         </BrowserRouter >
